075ccc14adb529e05287bd00d6763e32a09fd25a
Rusei Sakata <c612101505@tokushima-u.ac.jp>
2021-07-19 23:22:48 +0900

    避けるフェーズです。アニメーションの部分はコメント(//の後ろにある文章)の部分に書いてあります。

--- Assets/C#/yokeru.cs
+++ Assets/C#/yokeru.cs
@@ -6,32 +6,41 @@ public class yokeru : MonoBehaviour
 {
     // Start is called before the first frame update
     private int a, b;
+    //private Animator anim;
 
     void Start()
     {
-
+      //anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
       b=Random.Range(1,5);
+      //Debug.Log(b);
+      //a =0;
+      /*敵の入力を1~4の整数で入力してください*/
 
 
-    //ここが自分の避けるむきの入力をする場所
+      //ここが自分の入力
+
         if(Input.GetKeyDown(KeyCode.LeftArrow)){
           a = 1;
+          //anim.SetBool("左むく",true);
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow)){
           a = 2;
+          //anim.SetBool("右向く",true);
         }
         else if(Input.GetKeyDown(KeyCode.UpArrow)){
           a = 3;
+          //anim.SetBool("上向く",true);
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow)){
           a = 4;
+          //anim.SetBool("下向く",true);
         }
-      //}
+
         else  {
           Debug.Log("矢印を入力してください");
         }
@@ -45,7 +54,7 @@ public class yokeru : MonoBehaviour
 
         }
         else if(a!=b){
-            Debug.Log("ここでじゃんけんのシーンにとぶ");
+            Debug.Log("ここでじゃんけんのシーンに飛ぶ");
 
             a = 0;
         }
