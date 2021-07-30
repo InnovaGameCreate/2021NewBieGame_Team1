f6d75b17791465f399996337978b758836f9830d
NagataKentaro <c612101384@tokushima-u.ac.jp>
2021-07-31 06:15:51 +0900

    変更の破棄をしました

--- Assets/C#/yokeru.cs
+++ Assets/C#/yokeru.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SceneManagement;
-
 public class yokeru : MonoBehaviour
 {
     // Start is called before the first frame update
