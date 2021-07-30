f13a2829d6c19f62fcc0d3776988453cfe32ce13
IwamotoHinata <c612101277@tokushima-u.ac.jp>
2021-07-31 06:55:05 +0900

    少しSEのスクリプトをいじりました。

--- Assets/C#/Sound/SE.cs
+++ Assets/C#/Sound/SE.cs
@@ -4,34 +4,50 @@ using UnityEngine;
 
 public class SE : MonoBehaviour
 {
-
-    public AudioClip sound1;
+     float reload;
+     float reloadTime1;
+     float reloadTime2;
     AudioSource audioSource;
-
-    private bool once;
 
     void Start()
     {
-        //Component‚ğæ“¾
-        once = true;
+        //Component???
         audioSource = GetComponent<AudioSource>();
+       reload = 1f;
+       reloadTime1 = 0.5f;
+       reloadTime2 = 0.1f;
     }
 
     void Update()
     {
-        // SpaceƒL[
-        if (Input.GetKey(KeyCode.Space) || once == true)
-        {
-            //SE‚ğ–Â‚ç‚·
-            audioSource.PlayOneShot(sound1);
-            once = false;
+         if(reloadTime1<reload)
+          {
+              reloadTime1 = reloadTime1 + Time.deltaTime;
+          }
+            else if(Input.GetKey(KeyCode.Space))
+              {
+                    audioSource.Play();
+                    reloadTime1=0f;
+              }
+            else if(Input.GetKey(KeyCode.E))
+                {
+                    audioSource.Play();
+                    reloadTime1=0f;
+                }
 
-        } 
-
-        if (Input.GetKey(KeyCode.E) || once == true)
-        {
-            audioSource.PlayOneShot(sound1);
-            once = false;
-        }
-    }
-}
+                if(reloadTime2<reload)
+                {
+                    reloadTime2 = reloadTime2 + Time.deltaTime;
+                }
+              else if(Input.GetKey(KeyCode.RightArrow))
+                {
+                    audioSource.Play();
+                    reloadTime2=0f;
+                }
+              else if(Input.GetKey(KeyCode.LeftArrow))
+                {
+                    audioSource.Play();
+                    reloadTime2=0f;
+                }  
+    }       
+} 
