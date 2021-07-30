9a0e28edaa06f6c9d4525bc90cb0312841bb7959
NagataKentaro <c612101384@tokushima-u.ac.jp>
2021-07-31 02:45:10 +0900

    イライラゲージをシーン間で共有できるようになりました じゃんけんに戻る時はまだです。

---
+++ Assets/C#/IrairaGauge/GaugeDirector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeDirector : MonoBehaviour
+{
+
+    private Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.fillAmount = GameObject.Find("IrairaDirector").GetComponent<IrairaDirector>().GetFillAmount();
+    }
+    void Update()
+    {
+        
+    }
+}
